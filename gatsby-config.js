e6ace002203cd7a039dc288dd9dfeab7d5650753
julien210 <57074108+julien210@users.noreply.github.com>
2021-02-26 17:14:39 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
           verbose: true,
           schema : {
             timeout: 200000,
-            requestConcurrency: 10,
+            requestConcurrency: 8,
           },
       },
     },
